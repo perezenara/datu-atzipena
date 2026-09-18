0d68d99e203174b17c0da876cebfcbb624df4df0
perezenara <perez.enara@uni.eus>
2026-09-18 11:46:49 +0200

    Maiuskulak

---
+++ 1-Fitxategiak/iostreams/src/main/java/paagbi/CopyCharacterMaiuskula.java
@@ -0,0 +1,30 @@
+package paagbi;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyCharacterMaiuskula {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("1-Fitxategiak/iostreams/xanadu.txt");
+            out = new FileOutputStream("outagain.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                
+                out.write(Character.toUpperCase((char) c));
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
