f406eb425ecb157bfc0dbb0f1d0cccd1e9ff9a52
enara <perez.enara@uni.eus>
2026-09-18 10:28:56 +0200

    Update and rename CopyBytesOrdezkatu.java to CopyCharacterOrdezkatu.java

--- 1-Fitxategiak/iostreams/src/main/java/paagbi/CopyCharacterOrdezkatu.java
+++ 1-Fitxategiak/iostreams/src/main/java/paagbi/CopyCharacterOrdezkatu.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class CopyBytesOrdezkatu {
+public class CopyCharacterOrdezkatu {
     public static void main(String[] args) throws IOException {
 
         FileInputStream in = null;
