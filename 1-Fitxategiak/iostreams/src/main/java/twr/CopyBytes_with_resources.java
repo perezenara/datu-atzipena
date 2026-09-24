78ca3de44e0b1a8afc9c18faee648dbdc921b476
perezenara <perez.enara@uni.eus>
2026-09-24 10:48:34 +0200

    Try-with-resources formato egokian

--- 1-Fitxategiak/iostreams/src/main/java/twr/CopyBytes_with_resources.java
+++ 1-Fitxategiak/iostreams/src/main/java/twr/CopyBytes_with_resources.java
@@ -1,16 +1,14 @@
-package paagbi;
+package twr;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class CopyBytes {
+public class CopyBytes_with_resources {
     public static void main(String[] args) throws IOException {
 
-        FileInputStream in = null;
-        FileOutputStream out = null;
 
-        try (in = new FileInputStream("1-Fitxategiak/iostreams/xanadu.txt");
-            out = new FileOutputStream("outagain.txt")) {
+        try (   FileInputStream in = new FileInputStream("1-Fitxategiak/iostreams/xanadu.txt");
+                FileOutputStream out = new FileOutputStream("outagain.txt");) {
             int c;
 
             while ((c = in.read()) != -1) {
