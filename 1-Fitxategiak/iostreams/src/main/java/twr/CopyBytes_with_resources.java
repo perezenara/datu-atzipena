1e72d2f9208ce1cb3694a003d9b9fa7439cfa38e
perezenara <perez.enara@uni.eus>
2026-09-24 11:46:02 +0200

    Definitibo Try-with-resources

--- 1-Fitxategiak/iostreams/src/main/java/twr/CopyBytes_with_resources.java
+++ 1-Fitxategiak/iostreams/src/main/java/twr/CopyBytes_with_resources.java
@@ -1,11 +1,12 @@
 package twr;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CopyBytes_with_resources {
     public static void main(String[] args) throws IOException {
-
+        System.out.println("Ruta erlatiboen jatorria: "+new File( "").getAbsolutePath());
 
         try (   FileInputStream in = new FileInputStream("1-Fitxategiak/iostreams/xanadu.txt");
                 FileOutputStream out = new FileOutputStream("outagain.txt");) {
